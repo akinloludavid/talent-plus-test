a89664f02db76d94bccb3e908dff89d2450f62f7
Akinlolu Ogunleye <akinloludavid2727@gmail.com>
2023-03-29 08:29:39 +0100

    added smooth scroll

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,12 @@
 import { Image } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Logo = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Image
+        onClick={() => navigate("/")}
         cursor={"pointer"}
         src="/assets/logo.svg"
         w={["53px", "53px", "130px"]}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
-import { useRoutes } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useRoutes } from "react-router-dom";
 import PageLayout from "../components/PageLayout";
 import { AppRoutes } from "./routes";
 
@@ -8,6 +9,13 @@ export const AppRouteWrapper = () => {
 };
 
 export const Pages = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
   return (
     <PageLayout>
       <AppRouteWrapper />
