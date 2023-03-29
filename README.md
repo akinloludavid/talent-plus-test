606de74cf4b86dfbdec6a416ee3beb99a3f8ec85
Akinlolu Ogunleye <akinloludavid2727@gmail.com>
2023-03-29 03:34:49 +0100

    setup docker

---
+++ README.md
@@ -0,0 +1,3 @@
+To run docker image
+Run the command `docker-compose up --build`
+The docker image and the container will be built and the app can be accessed on http://localhost:5173
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,6 +9,7 @@ import {
   Input,
   InputRightElement,
   Button,
+  Box,
 } from "@chakra-ui/react";
 import React from "react";
 import {
@@ -36,51 +37,53 @@ const Footer = () => {
           "repeat(1,1fr)",
           "repeat(1,1fr)",
           "repeat(2,1fr)",
-          "repeat(3,1fr)",
-
-          "repeat(5,1fr)",
+          "repeat(6,1fr)",
+          "repeat(9,1fr)",
         ]}
+        w="full"
         columnGap="80px"
         rowGap={"32px"}
       >
-        <GridItem>
+        <GridItem colSpan={[1, 1, 2]}>
           <Logo />
           <Text color="white" fontSize={"16px"} fontWeight="400" my="26px">
             Reach out to us on any of our social media networks
           </Text>
-          <Flex gap="30px">
-            <FaFacebook color="#2A2A2B" size="32px" />
-            <FaTwitter color="#2A2A2B" size="32px" />
-            <FaYoutube color="#2A2A2B" size="32px" />
-            <FaInstagram color="#2A2A2B" size="32px" />
-            <FaDiscord color="#2A2A2B" size="32px" />
+          <Flex gap="30px" w="full">
+            <FaFacebook cursor={"pointer"} color="#2A2A2B" size="32px" />
+            <FaTwitter cursor={"pointer"} color="#2A2A2B" size="32px" />
+            <FaYoutube cursor={"pointer"} color="#2A2A2B" size="32px" />
+            <FaInstagram cursor={"pointer"} color="#2A2A2B" size="32px" />
+            <FaDiscord cursor={"pointer"} color="#2A2A2B" size="32px" />
           </Flex>
         </GridItem>
-        <GridItem>
-          <Heading
-            fontSize={["24px", "24px", "32px"]}
-            fontWeight="600"
-            color="white"
-            mb="32px"
-          >
-            Useful Links
-          </Heading>
-          <Flex flexDirection={["column"]} gap="16px">
-            {navLinks.map((el) => (
-              <Link
-                color="white"
-                key={el.label}
-                fontSize={["16px", "16px", "24px"]}
-                fontWeight="400"
-                _hover={{ textDecoration: "none" }}
-                onClick={() => navigate(el.route)}
-              >
-                {el.label}
-              </Link>
-            ))}
-          </Flex>
+        <GridItem colSpan={[1, 1, 2]}>
+          <Box w="100%">
+            <Heading
+              fontSize={["24px", "24px", "32px"]}
+              fontWeight="600"
+              color="white"
+              mb="32px"
+            >
+              Useful Links
+            </Heading>
+            <Flex flexDirection={["column"]} gap="16px">
+              {navLinks.map((el) => (
+                <Link
+                  color="white"
+                  key={el.label}
+                  fontSize={["16px", "16px", "24px"]}
+                  fontWeight="400"
+                  _hover={{ textDecoration: "none" }}
+                  onClick={() => navigate(el.route)}
+                >
+                  {el.label}
+                </Link>
+              ))}
+            </Flex>
+          </Box>
         </GridItem>
-        <GridItem>
+        <GridItem colSpan={[1, 1, 2]}>
           <Heading
             fontSize={["24px", "24px", "32px"]}
             fontWeight="600"
@@ -104,7 +107,7 @@ const Footer = () => {
             ))}
           </Flex>
         </GridItem>
-        <GridItem colSpan={[1, 1, 1, 2]}>
+        <GridItem colSpan={[1, 1, 1, 3]}>
           <Heading
             fontSize={["24px", "24px", "32px"]}
             fontWeight="600"
@@ -113,9 +116,11 @@ const Footer = () => {
           >
             Subscribe Us
           </Heading>
-          <InputGroup w="full">
+          <InputGroup w="full" h="46px">
             <Input
               outline="0px"
+              color="white"
+              h="full"
               _focus={{
                 outline: "0px",
               }}
@@ -130,7 +135,20 @@ const Footer = () => {
               border="1px solid #054FB3"
             />
             <InputRightElement w="auto">
-              <Button px="15.5px" h="100%" fontSize={"14px"} fontWeight="500">
+              <Button
+                my="auto"
+                borderRadius="0px 2px 2px 0px"
+                px="15.5px"
+                _hover={{}}
+                _active={{}}
+                py="13.5px"
+                fontSize={"14px"}
+                fontWeight="500"
+                bg="pryColor"
+                color="white"
+                h="46px"
+                variant={"solid"}
+              >
                 send message
               </Button>
             </InputRightElement>
